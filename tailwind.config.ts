6948fa05e85081db655ddf807143436346268db7
m-gie <maciejglowacki0@gmail.com>
2024-11-04 15:10:26 +0100

    chore: fonts setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
     },
   },
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [],
 };
 
 export default config;
