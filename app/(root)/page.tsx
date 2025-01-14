eeabd1a7a9997d364ab29c63516f15a24aab37b3
Cienias <cieja0@gmail.com>
2025-01-14 13:30:18 +0100

    feat: add social auth with Google and Github

---
+++ app/(root)/page.tsx
@@ -0,0 +1,26 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <div>
+      <div className="text-3xl font-black ">Hello!</div>
+      <div className="font-space-grotesk text-3xl ">Hello!</div>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit"> Log Out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
