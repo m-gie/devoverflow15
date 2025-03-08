d029c7bd92bd7a01d66075f0909c0abe95740425
Cienias <cieja0@gmail.com>
2025-03-08 18:21:44 +0100

    feat: implement social auth

---
+++ app/api/auth/signin-with-oauth/route.ts
@@ -0,0 +1,80 @@
+import { Account } from "@/database";
+import User from "@/database/user.model";
+import dbConnect from "@/lib/mongoose";
+import { SignInWithOAuthSchema } from "@/lib/validations";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+import slugify from "slugify";
+
+export async function POST(request: Request) {
+  const { provider, providerAccountId, user } = await request.json();
+  await dbConnect();
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const validatedData = SignInWithOAuthSchema.safeParse({
+      provider,
+      providerAccountId,
+      user,
+    });
+    if (!validatedData.success) {
+      return NextResponse.json(
+        { success: false, data: validatedData.error.flatten().fieldErrors },
+        { status: 400 }
+      );
+    }
+    const { name, username, email, image } = user;
+    const slugifiedUsername = slugify(username, {
+      lower: true,
+      strict: true,
+      trim: true,
+    });
+    let existingUser = await User.findOne({ email }).session(session);
+    if (!existingUser) {
+      [existingUser] = await User.create(
+        [{ name, slugifiedUsername, email, image }],
+        { session }
+      );
+    } else {
+      const updatedData: { name?: string; image?: string } = {};
+      if (existingUser.name !== name) updatedData.name = name;
+      if (existingUser.image !== image) updatedData.image = image;
+      if (Object.keys(updatedData).length > 0) {
+        await User.updateOne(
+          { _id: existingUser._id },
+          { $set: updatedData }
+        ).session(session);
+      }
+    }
+
+    const existingAccount = await Account.findOne({
+      userId: existingUser._id,
+      provider,
+      providerAccountId,
+    }).session(session);
+
+    if (!existingAccount) {
+      await Account.create(
+        [
+          {
+            userId: existingUser._id,
+            name,
+            image,
+            provider,
+            providerAccountId,
+          },
+        ],
+        { session }
+      );
+    }
+
+    await session.commitTransaction();
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    await session.abortTransaction();
+    return NextResponse.json({ success: false, data: error }, { status: 500 });
+  } finally {
+    session.endSession();
+  }
+}
